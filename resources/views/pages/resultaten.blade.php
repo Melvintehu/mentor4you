4104bb26c771ac4ba52fad545c94eba0f4c3db57
Melvin Tehubijuluw <melvindrachten@hotmail.com>
2016-07-04 14:11:31 +0200

    resultaten

--- resources/views/pages/resultaten.blade.php
+++ resources/views/pages/resultaten.blade.php
@@ -15,11 +15,55 @@
 
 			</div>
 
-			<div class="col-lg-3 push-35">
+			<div class="col-lg-12 no-overflow ">
+
+				<input class="input border border-accent right" type="text" placeholder="filteren" >
+
+			</div>
+
+			<div class="col-lg-3 push-35 relative ">
 				
 				<div class="image">
 					
 					<img src="images/resultaten5.jpg" alt="leerling">
+
+				</div>
+				<!-- Overlay -->
+				<div class="overlay">
+
+					<div class="overlay-resultaten bg-secondary">
+						
+						<div class="col-lg-12 push-aside-65 push-65">
+						
+							<h2 class="white "> MENTOR  </h2>
+
+						</div>
+
+						<div class="col-lg-3">
+							
+
+						</div>
+
+						<div class="col-lg-9">
+							
+							<p class="text white ">
+								
+								Morbi elementum, quam ut suscipit elementum, risus libero tincidunt est, auctor 
+								imperdiet elit felis vel turpis. Ut ac nulla eget neque tincidunt rutrum. Sed varius 
+								dictum elit, ac scelerisque lorem pharetra eu. Ut pellentesque, diam vitae consequat 
+								elementum, ligula ipsum viverra elit, sed pharetra nunc purus sit amet dolor. Nulla 
+								facilisi. Aliquam rutrum orci ut turpis laoreet, eget convallis tellus ornare. Maecenas 
+								at magna quis turpis posuere varius. Vivamus tincidunt tortor a est venenatis, sed 
+								eleifend magna fringilla. Integer eget ante in lacus convallis sagittis in in massa. 
+								Duis metus ex, faucibus sed sapien vestibulum, tempus gravida nunc.
+
+							</p>
+
+						</div>
+
+					</div>
+					
+					
 
 				</div>
 
