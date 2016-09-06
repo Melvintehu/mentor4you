835cf7ce6c35a0eeecefaa73324aa6830b42c554
Elandlord <e.landheer@mentechmedia.nl>
2016-09-06 17:37:54 +0200

    aaa
    aaa

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -119,7 +119,7 @@ class PagesController extends Controller
 	{
         $data = [
             'titel' => Section::where('id', 14)->first(),
-            'nieuwsberichten' => News::paginate(10),
+            'nieuwsberichten' => News::paginate(2),
             'sliders' => $this->getSliders(),
         ];
 		return view('pages.actueel', compact('data'));
