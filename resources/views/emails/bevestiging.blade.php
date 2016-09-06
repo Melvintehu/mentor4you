f1257a26aeeefbbe6568d262f2b3b750424bfe60
Melvin Tehubijuluw <melvindrachten@hotmail.com>
2016-09-06 23:13:35 +0200

    Commit

---
+++ resources/views/emails/bevestiging.blade.php
@@ -0,0 +1,16 @@
+@extends('beautymail::templates.sunny')
+
+@section('content')
+
+    @include ('beautymail::templates.sunny.heading' , [
+        'heading' => 'Bedankt voor uw bericht.',
+        'level' => 'h1',
+    ])
+
+    @include('beautymail::templates.sunny.contentStart')
+
+        <p>Wij proberen zo spoedig mogelijk contact op te nemen.</p>
+
+    @include('beautymail::templates.sunny.contentEnd')
+
+@stop
