faecbe7f16a19052d50f5f53451029a43b4df0ca
Melvin Tehubijuluw <melvindrachten@hotmail.com>
2016-11-05 13:48:00 +0100

    Nieuw logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,7 +12,7 @@
 				
 				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 main-logo hidden-xs ">
 
-					<img class="main-logo" src='../images/mentor4you-logo.jpg' />
+					<img class="main-logo" src='../images/mentor4you-logo-web.png' />
 
 				</div>
 
